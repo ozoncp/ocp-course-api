076e6e18c040fd2275057168f31d7e48f418bd9e
Aleksei Shashev <ashashev@gmail.com>
2021-05-25 20:03:57 +0300

    It looks give more performance

--- internal/utils/slice/GenerateWindows_generic.go
+++ internal/utils/slice/GenerateWindows_generic.go
@@ -6,11 +6,7 @@ import "github.com/ozoncp/ocp-course-api/internal/utils/commons"
 
 func GenerateWindowsTValue(xs []TValue, size int, step int) [][]TValue {
 	srcLen := len(xs)
-	dstLen := srcLen / step
-	if (srcLen % step) > 0 {
-		dstLen++
-	}
-	res := make([][]TValue, 0, dstLen)
+	res := make([][]TValue, 0)
 	for i := 0; i < srcLen; i += step {
 		end := commons.MinInt(i+size, srcLen)
 		res = append(res, xs[i:end:end])
